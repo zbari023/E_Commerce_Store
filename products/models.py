a6118bb5ea29e666499b9a23ad89a5feacaaaa76
zbari023 <barry450643@gmail.com>
2023-06-26 21:59:51 +0200

    neu work in models and analysis

--- products/models.py
+++ products/models.py
@@ -6,13 +6,20 @@ class Products(models.Model):
     description = models.TextField(max_length=30000)
     sku = models.IntegerField()
     price = models.FloatField()
+    subtitle = models.TextField(max_length=600)
+    brand = models.ForeignKey('Brand',on_delete=models.CASCADE,related_name='product_brand')
+    def __str__(self):
+        return self.name
     
 
 class ProductsImages(models.Model):
     pass 
 
 class Brand(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='brand')
+    def __str__(self):
+        return self.name
 
 class Reviews(models.Model):
     pass
