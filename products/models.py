6b53d4605ccbef6f6096f0ba9cba403db4f37fdf
zbari023 <barry450643@gmail.com>
2023-06-26 21:28:26 +0200

    add frontend

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=30000)
+    sku = models.IntegerField()
+    price = models.FloatField()
+    
+
+class ProductsImages(models.Model):
+    pass 
+
+class Brand(models.Model):
+    pass
+
+class Reviews(models.Model):
+    pass
