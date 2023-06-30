a3f00b01d4681e8dc99f700472187692f43cb9f9
zbari023 <barry450643@gmail.com>
2023-06-30 10:47:50 +0200

    add product admin customization

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, ProductsImages, Brand, Reviews
+
+
+class ProductsImagesAdmin(admin.TabularInline):
+    model= ProductsImages
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'flag','brand']
+    list_filter = ['price', 'flag','brand']
+    search_fields = ['name', 'subtitle']
+    inlines= [ProductsImagesAdmin,]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductsImages)
+admin.site.register(Brand)
+admin.site.register(Reviews)
