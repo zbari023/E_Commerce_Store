9ca60da7be73237b0a0374364f6a338d53e35d21
zbari023 <barry450643@gmail.com>
2023-07-01 18:03:02 +0200

    add product list ,detail with data

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProductList , ProductDetail
 
+app_name = 'products'
+
 urlpatterns = [
     path('',ProductList.as_view(),name = 'product_list'),
     path('<int:pk>',ProductDetail.as_view(),name = 'product_detail'),
--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls',namespace='products}'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
