40e36874ed5cc4a17a9ad181db31aa35815613c2
zbari023 <barry450643@gmail.com>
2023-07-10 21:30:53 +0200

    add brand_detail and brand_list

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('',ProductList.as_view(),name = 'product_list'),
     path('<slug:slug>',ProductDetail.as_view(),name = 'product_detail'),
     
-    path('brand',BrandList.as_view(),name = 'brand_list'),
+    path('brand/',BrandList.as_view(),name = 'brand_list'),
     path('brand/<slug:slug>',BrandDetail.as_view(),name = 'brand_detail'),
     
     
