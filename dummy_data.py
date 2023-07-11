29e01fbeed4cbf88943361310416dedec99a6276
zbari023 <barry450643@gmail.com>
2023-07-11 14:20:08 +0200

    add fake products

--- dummy_data.py
+++ dummy_data.py
@@ -13,13 +13,29 @@ def seed_brand(n):
     for x in range(n):
         Brand.objects.create(
             name = fake.name(),
-            image = f'brand/{images[random.randint(1, 25)]}'
-            
-            
+            image = f'brand/{images[random.randint(0,24)]}'
         )
     print(f'{n} Brands was Created succesfuly ...')
         
 def seed_product(n):
-    pass
+    fake = Faker()
+    images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg','22.jpg','23.jpg','24.jpg','25.jpg']
+    flag_types = ['New','Feature','Sale']
+    for x in range(n):  
+        Product.objects.create(
+            name = fake.name(),
+            description = fake.text(max_nb_chars=30000),
+            sku = random.randint(100,100000),
+            price = round(random.uniform(01.99,99.99),2),
+            subtitle = fake.text(max_nb_chars=600),
+            image = f'products/{images[random.randint(0,24)]}',
+            brand = Brand.objects.get(id=random.randint(0,283)),
+            flag = flag_types[random.randint(0,2)],
+            
+        )
+    print(f'{n} Products was Created succesfuly ...')
 
-seed_brand(2)
+
+# seed_brand(24)
+
+seed_product(2)
