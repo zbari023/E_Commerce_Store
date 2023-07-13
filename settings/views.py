32b7480c16a547cae47f23adc188b23de3b5ab47
zbari023 <barry450643@gmail.com>
2023-07-13 21:52:42 +0200

    add view file for home page

--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+from products.models import Product , Brand , Reviews
 # Create your views here.
 
 
 def home(request):
+    brands = Brand.objects.all()
+    sale_products = Product.objects.filter(flag='sale')
+    feature_products = Product.objects.filter(flag='feature')
+    new_products = Product.objects.filter(flag='New')
+    reviews = Reviews.objects.all()
     
-    return render(request,'settings/home.html',{})
+    return render(request,'settings/home.html',{
+        'brands':brands ,
+        'sale_products':sale_products,
+        'feature_products':feature_products,
+        'new_products':new_products,
+        'reviews':reviews,
+        
+    })
