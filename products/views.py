017b8f7144bd56245fc8f5c9e7e5f8c9848092e5
zbari023 <barry450643@gmail.com>
2023-07-27 22:41:16 +0200

    aggregate Max(price)

--- products/views.py
+++ products/views.py
@@ -12,7 +12,8 @@ def post_list_debug(request):   # QuerySet API reference
     # data = Product.objects.filter(price__lt=30.6)    # filter return as list 
     # data = Product.objects.all().order_by('name')
     # data = Product.objects.select_related('brand').all() # onetoone and onetomany
-    data = Product.objects.aggregate(Count('id'))
+    # data = Product.objects.aggregate(Count('id'))
+    data = Product.objects.aggregate(Max('price'))
     
     
     
