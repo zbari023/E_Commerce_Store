af4efa4ae678e2ff54d393e66f84ae32feb4725f
zbari023 <barry450643@gmail.com>
2023-07-27 23:18:57 +0200

    in home.html object.posts_count

--- settings/views.py
+++ settings/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from products.models import Product , Brand , Reviews
+from django.db.models.aggregates import Count
 # Create your views here.
 
 
 def home(request):
-    brands = Brand.objects.all()
+    brands = Brand.objects.all().annotate(posts_count=Count('product_brand'))
     sale_products = Product.objects.filter(flag='Sale')[:10]
     feature_products = Product.objects.filter(flag='Feature')[:6]
     new_products = Product.objects.filter(flag='New')[:10]
