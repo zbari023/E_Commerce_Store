1e1b51453f6b8fe336c485f73cb44eb7e3b512fe
zbari023 <barry450643@gmail.com>
2023-07-29 21:59:58 +0200

    add regitrierung html pages templates

--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,7 @@ MEDIA_ROOT = BASE_DIR / "media"     # --------------
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
