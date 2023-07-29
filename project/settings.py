341b0723513d9ba0c761db6294e3f77d74aaa0f1
zbari023 <barry450643@gmail.com>
2023-07-29 21:00:17 +0200

    add new app accounts

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'settings',
     'debug_toolbar',
+    
 ]
 
 MIDDLEWARE = [
