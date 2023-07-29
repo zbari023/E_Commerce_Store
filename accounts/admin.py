a00dbbce677624a10fd8e7438202fe7ce7591960
zbari023 <barry450643@gmail.com>
2023-07-29 23:24:12 +0200

    add signup signal and add signup , activateuser forms

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile , Phones , Address
+# from django.contrib.auth.models import User
+
+# Register your models here.
+
+# class MyUser(UserAdmin):
+#     pass
+
+
+
+
+
+class PhonesAdmin(admin.TabularInline):
+    model = Phones
+
+class AddressAdmin(admin.TabularInline):
+    model = Address
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    # inlines = [PhonesAdmin,AddressAdmin]
+    pass
+
+
+
+
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(Phones)
+admin.site.register(Address)
