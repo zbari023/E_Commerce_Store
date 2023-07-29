f3addbe117fb04d7c7c9fb9d32b230c49600aaf8
zbari023 <barry450643@gmail.com>
2023-07-29 22:11:23 +0200

    new in accounts templates

--- project/settings.py
+++ project/settings.py
@@ -144,4 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
