573541982f02bffd753ff8a54a1992bcd116f243
zbari023 <barry450643@gmail.com>
2023-08-15 17:35:24 +0200

    add coupons class to my model

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from accounts.models import Address
 from products.models import Product
 from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 
 # Create your models here.
 
@@ -18,20 +19,20 @@ ORDER_STATUS = (
 
 class Order(models.Model):
     user = models.ForeignKey(User,related_name='user_order' , on_delete=models.SET_NULL , null=True , blank=True)
-    code = models.CharField(max_length=30)
+    code = models.CharField(max_length=30 ,default=generate_code)
     status = models.CharField(max_length=30, choices=ORDER_STATUS)
     order_time = models.DateTimeField(default=timezone.now)
     delivery_time = models.DateTimeField(null=True , blank=True)
     delivery_location = models.ForeignKey(Address,related_name='delivery_address', on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
-        return str(self.user)
+        return self.code
     
     
 
 class OrderDetail(models.Model):
     order = models.ForeignKey(Order,related_name='order_detail' , on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='order_product', on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, related_name='order_product', on_delete=models.SET_NULL ,null=True)
     price = models.FloatField()
     quantity = models.IntegerField()
     total = models.FloatField()
@@ -50,10 +51,23 @@ class Cart(models.Model):
     
 class CartDetail(models.Model):
     cart = models.ForeignKey(Cart, related_name='cart_detail' , on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='cart_product', on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, related_name='cart_product', on_delete=models.SET_NULL ,null=True)
     price = models.FloatField()
     quantity = models.IntegerField()
     total = models.FloatField()
     
     def __str__(self):
         return str(self.product)
+    
+    
+
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=30)
+    percentage = models.FloatField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.code
