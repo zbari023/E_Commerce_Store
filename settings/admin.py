d3364b7bb28327ce3e3d644e584d6c1b5877cfdf
zbari023 <barry450643@gmail.com>
2023-08-15 19:38:52 +0200

    design for all classes in order

--- settings/admin.py
+++ settings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Company
+from .models import Company, DeliveryFee
 
 admin.site.register(Company)
+admin.site.register(DeliveryFee)
