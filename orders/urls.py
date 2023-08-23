8dc9c8aa6956772ab89ea9dc65e6361e7b3a0971
zbari023 <barry450643@gmail.com>
2023-08-23 13:34:11 +0200

    costumize order_list.html

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_to_cart
+from .views import add_to_cart , OrderList
 
 app_name = 'orders'
 urlpatterns = [
+    path('', OrderList.as_view()),
     path('add_to_cart', add_to_cart)
 ]
