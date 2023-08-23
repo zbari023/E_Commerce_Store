42e38e57210f08fb0dd2f26cc027d334b0243f18
zbari023 <barry450643@gmail.com>
2023-08-23 11:49:54 +0200

    add add_to_cart in view & template

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 # Create your views here.
 from django.views.generic import ListView
-from .models import Order
+from .models import Order , Cart ,CartDetail
+from products.models import Product
 
 
 
 class OrderList(ListView):
     model = Order
+
+
+def add_to_cart(request):
+    # get data from frontend
+    product = Product.objects.get(id=request.POST['product_id'])
+    quantity = request.POST['quantity']
+    # get cart data
+    cart = Cart.objects.get(user=request.user,completed=False)
+    cart_detai , created = CartDetail.objects.get_or_create(cart=cart , product=product)
+    cart_detail.quantity = quantity
+    cart_datail.price = product.price
+    cart_detail.total = int(quantity) * product.price
+    cart_detail.save()
+    return redirect(f'/products/{product.slug}')
