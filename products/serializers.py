89f6c2b1d6d44f3779adf0422106ea2d45e93c18
zbari023 <barry450643@gmail.com>
2023-08-30 21:13:33 +0200

    add in serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,2 +1,8 @@
 # get the Date from the model (DB) and convert its to json
+from rest_framework import serializers
+from .models import Product
 
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
