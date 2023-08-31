b02f97027ea94501b3912892f85c3b7d0c0344a7
zbari023 <barry450643@gmail.com>
2023-08-31 19:12:54 +0200

    add avg_rate in product list in api

--- products/serializers.py
+++ products/serializers.py
@@ -1,10 +1,12 @@
 # get the Date from the model (DB) and convert its to json
 from rest_framework import serializers
 from .models import Product , Brand
+from django.db.models.aggregates import Avg 
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     review_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
@@ -12,12 +14,17 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_review_count(self,object):
         review = object.product_review.all().count()
         return review
+    def get_avg_rate(self,object):
+        avg = object.product_review.aggregate(avg=Avg('rate'))
+        return avg
+
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = '__all__'
     
+
 
 class BrandDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(source='product_brand',many=True)
