2b74e00eaa4c4e0e794788e591836a64c769e840
zbari023 <barry450643@gmail.com>
2023-09-12 18:37:55 +0200

    add the filter in list api

--- products/api.py
+++ products/api.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .serializers import ProductSerializer , BrandSerializer, BrandDetailSerializer
 from .models import Product, Brand
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 """ @api_view(['GET'])
@@ -27,6 +27,8 @@ def product_detail_api(request,product_id):
 class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['brand', 'flag']
 
 class ProductDetailAPI(generics.RetrieveAPIView):
     queryset = Product.objects.all()
--- project/settings.py
+++ project/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 MIDDLEWARE = [
