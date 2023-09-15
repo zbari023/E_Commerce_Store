caa2c6e8dc7d25aeeeb8c446b23c50914c51656e
zbari023 <barry450643@gmail.com>
2023-09-15 21:27:09 +0200

    create the serializers classses

---
+++ orders/api.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from .models import *
+from .serializers import CartDetailSerializer , CartSerializer
+from django.contrib.auth.models import User
+
+
+
+
+
+
+
+
+
+
+
+        
+
