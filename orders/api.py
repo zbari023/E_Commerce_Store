c4367ee3e836263ae2905241a27db059d8d07df0
zbari023 <barry450643@gmail.com>
2023-09-15 21:52:49 +0200

    add in api class for cart as creating get function

--- orders/api.py
+++ orders/api.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import *
 from .serializers import CartDetailSerializer , CartSerializer
 from django.contrib.auth.models import User
+from rest_framework.response import Response
 
 
 
@@ -10,8 +11,18 @@ from django.contrib.auth.models import User
 
 
 
-
-
-
+class CartDetailCreateDeleteAPI(generics.GenericAPIView):
+    def get(self,request,*args,**kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        cart , created = Cart.objects.get_or_create(user=user,completed=False)
+        data = CartSerializer(cart).data
+        return Response({'Cart':data, 'Status':200})
         
+    def post(self,request,*args,**kwargs):
+        pass
+    
+    
+    
+    def delete(self,request,*args,**kwargs):
+        pass
 
--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import add_to_cart , OrderList
+from .api import CartDetailCreateDeleteAPI
 
 app_name = 'orders'
 urlpatterns = [
     path('', OrderList.as_view()),
-    path('add_to_cart', add_to_cart)
+    path('add_to_cart', add_to_cart),
+    path('api/<str:username>/cart',CartDetailCreateDeleteAPI.as_view())
 ]
