e37630c314b2e51d256be49f9996ed9027592051
zbari023 <barry450643@gmail.com>
2023-09-15 22:16:20 +0200

    add post function and delete in api class

--- orders/api.py
+++ orders/api.py
@@ -3,7 +3,7 @@ from .models import *
 from .serializers import CartDetailSerializer , CartSerializer
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-
+from products.models import *
 
 
 
@@ -19,10 +19,22 @@ class CartDetailCreateDeleteAPI(generics.GenericAPIView):
         return Response({'Cart':data, 'Status':200})
         
     def post(self,request,*args,**kwargs):
-        pass
-    
-    
+        user = User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.data['product_id'])
+        quantity = int(request.data['quantity'])
+        cart = Cart.objects.get(user=user ,completed=False)
+        cart_data , created = CartDetail.objects.get_or_create(cart=cart,product=product) 
+        cart_data.price = product.price
+        cart_data.quantity = quantity
+        cart_data.total = round(quantity*product.price,2)
+        cart_data.save()
+        return Response({'status':200,'message':'Product was added seccessfuly'})
     
     def delete(self,request,*args,**kwargs):
-        pass
+        user = User.objects.get(username=self.kwargs['username'])
+        cart = Cart.objects.get(user=user ,completed=False)
+        product = Product.objects.get(id=request.data['product_id'])
+        cart_detail = CartDetail.objects.get(cart=cart , product=product)
+        cart_detail.delete()
+        return Response({'status':200,'message':'Product was added seccessfuly deleted'})
 
--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from .views import add_to_cart , OrderList
 from .api import CartDetailCreateDeleteAPI
@@ -6,5 +7,7 @@ app_name = 'orders'
 urlpatterns = [
     path('', OrderList.as_view()),
     path('add_to_cart', add_to_cart),
-    path('api/<str:username>/cart',CartDetailCreateDeleteAPI.as_view())
+    path('api/<str:username>/cart',CartDetailCreateDeleteAPI.as_view(),)
 ]
+
+  
