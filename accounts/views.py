32a583fbe603d9b18f307f0a6a625e86bba0636f
zbari023 <barry450643@gmail.com>
2023-09-29 23:04:32 +0200

    the figure of db from chartsjs

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from .forms import SignupForm , ActivateUser
 from .models import Profile , Phones , Address
-from products.models import Product , Reviews
+from products.models import Product , Reviews, Brand
 from orders.models import Order
 from django.contrib.auth.models import User
 
@@ -23,8 +23,18 @@ def dashboard(request):
     new_products = Product.objects.filter(flag='New').count()
     sale_products = Product.objects.filter(flag='Sale').count()
     feature_products = Product.objects.filter(flag='Feature').count()
+    brands = Brand.objects.all().count() 
+    products = Product.objects.all().count()
+    reviews = Reviews.objects.all().count()
+    user = User.objects.all().count()
+    orders = Order.objects.all().count()
     return render(request,'registration/dashboard.html',{
         'new_products':new_products ,
         'sale_products':sale_products ,
-        'feature_products':feature_products
+        'feature_products':feature_products,
+        'user':user,
+        'products':products,
+        'reviews':reviews,
+        'brands':brands,
+        'orders':orders
     })
