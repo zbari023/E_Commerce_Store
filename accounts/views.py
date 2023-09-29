7354714f2208a7e18152a73fd7d4fdf90741dc98
zbari023 <barry450643@gmail.com>
2023-09-29 23:16:01 +0200

    the figure of db from chartsjs

--- accounts/views.py
+++ accounts/views.py
@@ -31,10 +31,10 @@ def dashboard(request):
     users = User.objects.all().count()
     orders = Order.objects.all().count()
     
-    recieved = Order.objects.filter(ORDER_STATUS ='recieved ').count()
-    processed = Order.objects.filter(ORDER_STATUS ='processed').count()
-    shipped = Order.objects.filter(ORDER_STATUS ='shipped').count()
-    delivered = Order.objects.filter(ORDER_STATUS ='delivered').count()
+    recieved = Order.objects.filter(status ='recieved ').count()
+    processed = Order.objects.filter(status ='processed').count()
+    shipped = Order.objects.filter(status ='shipped').count()
+    delivered = Order.objects.filter(status ='delivered').count()
     
     return render(request,'registration/dashboard.html',{
         'new_products':new_products ,
