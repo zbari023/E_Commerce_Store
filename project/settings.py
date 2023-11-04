27e92602624987116754ff1c8c87b5fd9ee0c7dc
zbari023 <barry450643@gmail.com>
2023-11-04 18:52:45 +0100

    add the settings to send for user gmail to reset the passwort

--- project/settings.py
+++ project/settings.py
@@ -167,7 +167,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 from datetime import timedelta
 SIMPLE_JWT = {
@@ -188,3 +187,11 @@ LANGUAGES = [
 ]
 
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "barry450643@gmail.com"
+EMAIL_HOST_PASSWORD = "sxoeqiudvblmuzbv"
